c160fad3745fc99e7515c9aa6670773f574b8de3
xtranghero <xtrangerhero@gmail.com>
2018-02-08 17:01:45 +0800

    S outs added

--- bin/bash/andon.sh
+++ bin/bash/andon.sh
@@ -35,67 +35,91 @@ PROCESSLIST=$(mysql -h$CLOUDHOST -u $CLOUDUSER -p$CLOUDPASS $CLOUDDB -s<<<"SELEC
 
 L_hourly_AM(){ # Hourly Outs for Linearity
     
-    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT process_id, SUM(out_qty) AS out_qty, HOUR(DATE_ADD(date_time, INTERVAL -390 MINUTE)) + 1 AS fab_hour , count(*) AS num_moves FROM MES_OUT_DETAILS WHERE process_id = '$process' AND DATE(DATE_ADD(date_time, INTERVAL -390 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE)) GROUP BY process_id, HOUR(DATE_ADD(date_time, INTERVAL -390 MINUTE));" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-L-$process.csv 
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT process_id, SUM(out_qty) AS out_qty, HOUR(DATE_ADD(date_time, INTERVAL -390 MINUTE)) + 1 AS fab_hour , count(*) AS num_moves FROM MES_OUT_DETAILS WHERE process_id = '$process' AND DATE(DATE_ADD(date_time, INTERVAL -390 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE)) GROUP BY process_id, HOUR(DATE_ADD(date_time, INTERVAL -390 MINUTE));" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-hourlyouts-$process.csv 
     
-    cp ../draw/$DATE2EXTRACT-$shift-L-$process.csv ../L/$DATE2EXTRACT-$shift-L-$process.csv
+    cp ../draw/$DATE2EXTRACT-$shift-hourlyouts-$process.csv ../L/$DATE2EXTRACT-$shift-hourlyouts-$process.csv
 }
 
 L_hourly_PREPM(){ # Hourly Outs for Linearity
     
-    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT process_id, SUM(out_qty) AS out_qty, HOUR(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) + 1 AS fab_hour , count(*) AS num_moves FROM MES_OUT_DETAILS WHERE process_id = '$process' AND DATE(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE)) GROUP BY process_id, HOUR(DATE_ADD(date_time, INTERVAL -1110 MINUTE));" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-L-$process.csv 
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT process_id, SUM(out_qty) AS out_qty, HOUR(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) + 1 AS fab_hour , count(*) AS num_moves FROM MES_OUT_DETAILS WHERE process_id = '$process' AND DATE(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE)) GROUP BY process_id, HOUR(DATE_ADD(date_time, INTERVAL -1110 MINUTE));" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-hourlyouts-$process.csv 
     
-    cp ../draw/$DATE2EXTRACT-$shift-L-$process.csv ../L/$DATE2EXTRACT-$shift-L-$process.csv
+    cp ../draw/$DATE2EXTRACT-$shift-hourlyouts-$process.csv ../L/$DATE2EXTRACT-$shift-hourlyouts-$process.csv
 }
 
 L_hourly_POSTPM(){ # Hourly Outs for Linearity
     
-    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT process_id, SUM(out_qty) AS out_qty, HOUR(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) + 1 AS fab_hour , count(*) AS num_moves FROM MES_OUT_DETAILS WHERE process_id = '$process' AND DATE(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -1 DAY)) GROUP BY process_id, HOUR(DATE_ADD(date_time, INTERVAL -1110 MINUTE));" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-L-$process.csv 
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT process_id, SUM(out_qty) AS out_qty, HOUR(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) + 1 AS fab_hour , count(*) AS num_moves FROM MES_OUT_DETAILS WHERE process_id = '$process' AND DATE(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -1 DAY)) GROUP BY process_id, HOUR(DATE_ADD(date_time, INTERVAL -1110 MINUTE));" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-hourlyouts-$process.csv 
     
-    cp ../draw/$DATE2EXTRACT-$shift-L-$process.csv ../L/$DATE2EXTRACT-$shift-L-$process.csv
+    cp ../draw/$DATE2EXTRACT-$shift-hourlyouts-$process.csv ../L/$DATE2EXTRACT-$shift-hourlyouts-$process.csv
 }
 
 Y_outsAndScrap_AM(){ # Outs and Scrap for Yield per tool
 
-    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT A.eq_name AS eq_name, A.scrap_qty AS scrap_qty, B.out_qty AS out_qty FROM   (SELECT B.eq_name, SUM(A.scrap_qty) AS scrap_qty    FROM MES_SCRAP_DETAILS A      JOIN MES_EQ_INFO B  ON A.eq_id = B.eq_id     WHERE DATE(DATE_ADD(A.date_time, INTERVAL -390 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE))   AND A.process_id = '$process'     GROUP BY B.eq_name ) A JOIN   (SELECT B.eq_name, SUM(A.out_qty) AS out_qty     FROM MES_OUT_DETAILS A     JOIN MES_EQ_INFO B   ON A.eq_id = B.eq_id    WHERE DATE(DATE_ADD(A.date_time, INTERVAL -390 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE))   AND A.process_id = '$process'  GROUP BY B.eq_name ) B ON A.eq_name = B.eq_name;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-Y-$process.csv 
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT A.eq_name AS eq_name, A.scrap_qty AS scrap_qty, B.out_qty AS out_qty FROM   (SELECT B.eq_name, SUM(A.scrap_qty) AS scrap_qty    FROM MES_SCRAP_DETAILS A      JOIN MES_EQ_INFO B  ON A.eq_id = B.eq_id     WHERE DATE(DATE_ADD(A.date_time, INTERVAL -390 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE))   AND A.process_id = '$process'     GROUP BY B.eq_name ) A JOIN   (SELECT B.eq_name, SUM(A.out_qty) AS out_qty     FROM MES_OUT_DETAILS A     JOIN MES_EQ_INFO B   ON A.eq_id = B.eq_id    WHERE DATE(DATE_ADD(A.date_time, INTERVAL -390 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE))   AND A.process_id = '$process'  GROUP BY B.eq_name ) B ON A.eq_name = B.eq_name;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-outsAndScrap-$process.csv 
 
-    cp ../draw/$DATE2EXTRACT-$shift-Y-$process.csv ../Y/$DATE2EXTRACT-$shift-Y-$process.csv
+    cp ../draw/$DATE2EXTRACT-$shift-outsAndScrap-$process.csv ../Y/$DATE2EXTRACT-$shift-outsAndScrap-$process.csv
 }
 
 Y_outsAndScrap_PREPM(){ # Outs and Scrap for Yield per tool
 
-    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT A.eq_name AS eq_name, A.scrap_qty AS scrap_qty, B.out_qty AS out_qty FROM   (SELECT B.eq_name, SUM(A.scrap_qty) AS scrap_qty    FROM MES_SCRAP_DETAILS A      JOIN MES_EQ_INFO B  ON A.eq_id = B.eq_id     WHERE DATE(DATE_ADD(A.date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE))   AND A.process_id = '$process'     GROUP BY B.eq_name ) A JOIN   (SELECT B.eq_name, SUM(A.out_qty) AS out_qty     FROM MES_OUT_DETAILS A     JOIN MES_EQ_INFO B   ON A.eq_id = B.eq_id    WHERE DATE(DATE_ADD(A.date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE))   AND A.process_id = '$process'  GROUP BY B.eq_name ) B ON A.eq_name = B.eq_name;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-Y-$process.csv 
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT A.eq_name AS eq_name, A.scrap_qty AS scrap_qty, B.out_qty AS out_qty FROM   (SELECT B.eq_name, SUM(A.scrap_qty) AS scrap_qty    FROM MES_SCRAP_DETAILS A      JOIN MES_EQ_INFO B  ON A.eq_id = B.eq_id     WHERE DATE(DATE_ADD(A.date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE))   AND A.process_id = '$process'     GROUP BY B.eq_name ) A JOIN   (SELECT B.eq_name, SUM(A.out_qty) AS out_qty     FROM MES_OUT_DETAILS A     JOIN MES_EQ_INFO B   ON A.eq_id = B.eq_id    WHERE DATE(DATE_ADD(A.date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE))   AND A.process_id = '$process'  GROUP BY B.eq_name ) B ON A.eq_name = B.eq_name;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-outsAndScrap-$process.csv 
 
-    cp ../draw/$DATE2EXTRACT-$shift-Y-$process.csv ../Y/$DATE2EXTRACT-$shift-Y-$process.csv
+    cp ../draw/$DATE2EXTRACT-$shift-outsAndScrap-$process.csv ../Y/$DATE2EXTRACT-$shift-outsAndScrap-$process.csv
 }
 
 Y_outsAndScrap_POSTPM(){ # Outs and Scrap for Yield per tool
 
-    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT A.eq_name AS eq_name, A.scrap_qty AS scrap_qty, B.out_qty AS out_qty FROM   (SELECT B.eq_name, SUM(A.scrap_qty) AS scrap_qty    FROM MES_SCRAP_DETAILS A      JOIN MES_EQ_INFO B  ON A.eq_id = B.eq_id     WHERE DATE(DATE_ADD(A.date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -1 DAY))   AND A.process_id = '$process'     GROUP BY B.eq_name ) A JOIN   (SELECT B.eq_name, SUM(A.out_qty) AS out_qty     FROM MES_OUT_DETAILS A     JOIN MES_EQ_INFO B   ON A.eq_id = B.eq_id    WHERE DATE(DATE_ADD(A.date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -1 DAY))   AND A.process_id = '$process'  GROUP BY B.eq_name ) B ON A.eq_name = B.eq_name" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-Y-$process.csv 
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT A.eq_name AS eq_name, A.scrap_qty AS scrap_qty, B.out_qty AS out_qty FROM   (SELECT B.eq_name, SUM(A.scrap_qty) AS scrap_qty    FROM MES_SCRAP_DETAILS A      JOIN MES_EQ_INFO B  ON A.eq_id = B.eq_id     WHERE DATE(DATE_ADD(A.date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -1 DAY))   AND A.process_id = '$process'     GROUP BY B.eq_name ) A JOIN   (SELECT B.eq_name, SUM(A.out_qty) AS out_qty     FROM MES_OUT_DETAILS A     JOIN MES_EQ_INFO B   ON A.eq_id = B.eq_id    WHERE DATE(DATE_ADD(A.date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -1 DAY))   AND A.process_id = '$process'  GROUP BY B.eq_name ) B ON A.eq_name = B.eq_name" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-outsAndScrap-$process.csv 
 
-    cp ../draw/$DATE2EXTRACT-$shift-Y-$process.csv ../Y/$DATE2EXTRACT-$shift-Y-$process.csv
+    cp ../draw/$DATE2EXTRACT-$shift-outsAndScrap-$process.csv ../Y/$DATE2EXTRACT-$shift-outsAndScrap-$process.csv
 }
 
 S_scrap_qty_AM(){ # Scrap qty for Scrap dppm
 
-    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT scrap_code, SUM(scrap_qty) AS scrap_qty FROM MES_SCRAP_DETAILS WHERE DATE(DATE_ADD(date_time, INTERVAL -390 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE)) AND process_id = '$process' GROUP BY scrap_code ORDER BY SUM(scrap_qty) DESC LIMIT 5;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-S-$process.csv 
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT scrap_code, SUM(scrap_qty) AS scrap_qty FROM MES_SCRAP_DETAILS WHERE DATE(DATE_ADD(date_time, INTERVAL -390 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE)) AND process_id = '$process' GROUP BY scrap_code ORDER BY SUM(scrap_qty) DESC LIMIT 5;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-scrap-$process.csv 
 
-    cp ../draw/$DATE2EXTRACT-$shift-S-$process.csv ../S/$DATE2EXTRACT-$shift-S-$process.csv
+    cp ../draw/$DATE2EXTRACT-$shift-scrap-$process.csv ../S/$DATE2EXTRACT-$shift-scrap-$process.csv
 
 }
 
 S_scrap_qty_PREPM(){ # Scrap qty for Scrap dppm
 
-    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT scrap_code, SUM(scrap_qty) AS scrap_qty FROM MES_SCRAP_DETAILS WHERE DATE(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE)) AND process_id = '$process' GROUP BY scrap_code ORDER BY SUM(scrap_qty) DESC LIMIT 5;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-S-$process.csv 
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT scrap_code, SUM(scrap_qty) AS scrap_qty FROM MES_SCRAP_DETAILS WHERE DATE(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -0 MINUTE)) AND process_id = '$process' GROUP BY scrap_code ORDER BY SUM(scrap_qty) DESC LIMIT 5;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-scrap-$process.csv 
 
-    cp ../draw/$DATE2EXTRACT-$shift-S-$process.csv ../S/$DATE2EXTRACT-$shift-S-$process.csv
+    cp ../draw/$DATE2EXTRACT-$shift-scrap-$process.csv ../S/$DATE2EXTRACT-$shift-scrap-$process.csv
 
 }
 
 S_scrap_qty_POSTPM(){ # Scrap qty for Scrap dppm
 
-    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT scrap_code, SUM(scrap_qty) AS scrap_qty FROM MES_SCRAP_DETAILS WHERE DATE(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -1 DAY)) AND process_id = '$process' GROUP BY scrap_code ORDER BY SUM(scrap_qty) DESC LIMIT 5;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-S-$process.csv 
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT scrap_code, SUM(scrap_qty) AS scrap_qty FROM MES_SCRAP_DETAILS WHERE DATE(DATE_ADD(date_time, INTERVAL -1110 MINUTE)) = DATE(DATE_ADD('$DATE2EXTRACT', INTERVAL -1 DAY)) AND process_id = '$process' GROUP BY scrap_code ORDER BY SUM(scrap_qty) DESC LIMIT 5;" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-scrap-$process.csv 
 
-    cp ../draw/$DATE2EXTRACT-$shift-S-$process.csv ../S/$DATE2EXTRACT-$shift-S-$process.csv
+    cp ../draw/$DATE2EXTRACT-$shift-scrap-$process.csv ../S/$DATE2EXTRACT-$shift-scrap-$process.csv
+
+}
+
+S_out_qty_AM(){
+
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT A.proc_id , SUM(C.out_qty) AS out_qty FROM		 (SELECT eq_id, proc_id  FROM MES_EQ_PROCESS   GROUP BY eq_id ) A     JOIN   MES_EQ_INFO B   ON A.eq_id = B.eq_id   JOIN   MES_OUT_DETAILS C     ON A.eq_id = C.eq_id   WHERE C.process_id = '$process' AND C.date_time >= CONCAT('$DATE2EXTRACT',' 06:30:00') && C.date_time <= CONCAT('$DATE2EXTRACT' + INTERVAL 0 DAY,' 18:29:59');" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-outs-$process.csv 
+
+    cp ../draw/$DATE2EXTRACT-$shift-outs-$process.csv ../S/$DATE2EXTRACT-$shift-outs-$process.csv
+
+}
+
+S_out_qty_PREPM(){
+
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT A.proc_id , SUM(C.out_qty) AS out_qty FROM		 (SELECT eq_id, proc_id  FROM MES_EQ_PROCESS   GROUP BY eq_id ) A     JOIN   MES_EQ_INFO B   ON A.eq_id = B.eq_id   JOIN   MES_OUT_DETAILS C     ON A.eq_id = C.eq_id   WHERE C.process_id = '$process' AND C.date_time >= CONCAT('$DATE2EXTRACT', '18:30:00') && C.date_time <= CONCAT('$DATE2EXTRACT' + INTERVAL 0 DAY,' 23:59:59');" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-outs-$process.csv 
+
+    cp ../draw/$DATE2EXTRACT-$shift-outs-$process.csv ../S/$DATE2EXTRACT-$shift-outs-$process.csv
+
+}
+
+S_out_qty_POSTPM(){
+
+    mysql -h$MESHOST -u $MESUSER -p$MESPASS $MESDB -e "SET time_zone = '+08:00'; SELECT A.proc_id , SUM(C.out_qty) AS out_qty FROM		 (SELECT eq_id, proc_id  FROM MES_EQ_PROCESS   GROUP BY eq_id ) A     JOIN   MES_EQ_INFO B   ON A.eq_id = B.eq_id   JOIN   MES_OUT_DETAILS C     ON A.eq_id = C.eq_id   WHERE C.process_id = '$process' AND C.date_time >= CONCAT('$DATE2EXTRACT' + INTERVAL -1 DAY, ' 18:30:00') && C.date_time <= CONCAT('$DATE2EXTRACT' + INTERVAL 0 DAY,' 06:29:59');" | sed 's/\t/,/g' > ../draw/$DATE2EXTRACT-$shift-outs-$process.csv 
+
+    cp ../draw/$DATE2EXTRACT-$shift-outs-$process.csv ../S/$DATE2EXTRACT-$shift-outs-$process.csv
 
 }
 
@@ -111,6 +135,7 @@ do
         L_hourly_AM # AM "Hourly Outs"
         Y_outsAndScrap_AM # AM "Outs and Scrap"
         S_scrap_qty_AM # Scrap qty
+        S_out_qty_AM # Outs qty
 
     elif (( $now > $PM_start_of_shift  )) && (( $now < $PM_premid_shift )) ; then
         shift='PM'
@@ -119,6 +144,8 @@ do
         L_hourly_PREPM # PREPM Hourly Outs
         Y_outsAndScrap_PREPM # PREPM "Outs and Scrap"
         S_scrap_qty_PREPM # Scrap qty
+        S_out_qty_PREPM # Outs qty
+
 
     elif (( $now > $PM_postmid_shift )) && (( $now < $PM_end_of_shift )); then
         shift='PM'
@@ -127,6 +154,8 @@ do
         L_hourly_POSTPM # POSTPM hourly outs
         Y_outsAndScrap_POSTPM # POSTPM "Outs and Scrap"
         S_scrap_qty_POSTPM # Scrap qty
+        S_out_qty_POSTPM # Outs qty
+
         
     fi
 
